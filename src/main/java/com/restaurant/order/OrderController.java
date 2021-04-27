36dd6cc8e82b0b53162016ade7ab25f3633853a0
Anoob-21 <an.80.2021@gmail.com>
2021-04-27 08:40:16 +0530

    Added Config server

--- src/main/java/com/restaurant/order/OrderController.java
+++ src/main/java/com/restaurant/order/OrderController.java
@@ -4,6 +4,8 @@ import com.restaurant.order.dto.RestaurantOrderDto;
 import com.restaurant.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +13,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/restaurantorder")
 @Slf4j
+@RefreshScope
 public class OrderController {
 
     @Autowired
     OrderService orderService;
 
+    @Value("${message:Hello default}")
+    private String message;
 
     @GetMapping("/getOrder")
     public ResponseEntity<RestaurantOrderDto> getOrder
             (@RequestParam(value="consumerId") String consumerId) {
 
+        log.info("Received message is : {}",message);
         log.info("Calling order service for consumer id : {}",consumerId);
 
         RestaurantOrderDto restaurantOrderDto = orderService.getOrder(consumerId);
